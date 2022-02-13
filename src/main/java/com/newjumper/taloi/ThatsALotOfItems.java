cb5c84b39d162a9080e4948ff8a6d604c97e9763
muhta <muhtasim.nabi@gmail.com>
2022-02-13 11:03:45 -0600

    stuff

--- src/main/java/com/newjumper/taloi/ThatsALotOfItems.java
+++ src/main/java/com/newjumper/taloi/ThatsALotOfItems.java
@@ -16,12 +16,9 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(ThatsALotOfItems.MOD_ID)
 public class ThatsALotOfItems {
     public static final String MOD_ID = "taloi";
-
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public ThatsALotOfItems() {
@@ -32,7 +29,6 @@ public class ThatsALotOfItems {
 
         eventBus.addListener(this::setup);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
