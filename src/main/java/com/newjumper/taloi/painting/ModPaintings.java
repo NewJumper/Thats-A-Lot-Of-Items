52e435e406c5a4e62198ea25822cc32b2b7fa28e
muhta <muhtasim.nabi@gmail.com>
2022-03-14 23:31:14 -0500

    added The Process painting, translated futurite apple, re-colored a pixel for the futurite apple

--- src/main/java/com/newjumper/taloi/painting/ModPaintings.java
+++ src/main/java/com/newjumper/taloi/painting/ModPaintings.java
@@ -11,6 +11,7 @@ public class ModPaintings {
     public static final DeferredRegister<Motive> PAINTING_MOTIVES = DeferredRegister.create(ForgeRegistries.PAINTING_TYPES, ThatsALotOfItems.MOD_ID);
 
     public static final RegistryObject<Motive> SPACE = PAINTING_MOTIVES.register("space", () -> new Motive(32, 32));
+    public static final RegistryObject<Motive> THE_PROCESS = PAINTING_MOTIVES.register("the_process", () -> new Motive(48, 48));
     public static final RegistryObject<Motive> TOMATO = PAINTING_MOTIVES.register("tomato", () -> new Motive(32, 32));
 
     public static void register(IEventBus eventBus) {
