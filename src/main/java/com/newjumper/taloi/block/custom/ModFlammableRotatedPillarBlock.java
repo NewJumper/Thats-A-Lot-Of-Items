82999501cff22395fbbf228759c316bb304da839
muhta <muhtasim.nabi@gmail.com>
2022-03-22 17:41:46 -0500

    wood update

--- src/main/java/com/newjumper/taloi/block/custom/ModFlammableRotatedPillarBlock.java
+++ src/main/java/com/newjumper/taloi/block/custom/ModFlammableRotatedPillarBlock.java
@@ -1,10 +1,18 @@
 package com.newjumper.taloi.block.custom;
 
+import com.newjumper.taloi.block.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.ToolAction;
+
+import javax.annotation.Nullable;
 
 public class ModFlammableRotatedPillarBlock extends RotatedPillarBlock {
     public ModFlammableRotatedPillarBlock(Properties properties) {
@@ -24,5 +32,26 @@ public class ModFlammableRotatedPillarBlock extends RotatedPillarBlock {
     @Override
     public int getFireSpreadSpeed(BlockState state, BlockGetter world, BlockPos pos, Direction face) {
         return 5;
+    }
+
+    @Nullable
+    @Override
+    public BlockState getToolModifiedState(BlockState state, Level world, BlockPos pos, Player player, ItemStack stack, ToolAction toolAction) {
+        if(stack.getItem() instanceof AxeItem) {
+            if(state.is(ModBlocks.EVERGREEN_LOG.get())) {
+                return ModBlocks.STRIPPED_EVERGREEN_LOG.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+            }
+            if(state.is(ModBlocks.EVERGREEN_WOOD.get())) {
+                return ModBlocks.STRIPPED_EVERGREEN_WOOD.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+            }
+            if(state.is(ModBlocks.WILLOW_LOG.get())) {
+                return ModBlocks.STRIPPED_WILLOW_LOG.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+            }
+            if(state.is(ModBlocks.WILLOW_WOOD.get())) {
+                return ModBlocks.STRIPPED_WILLOW_WOOD.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
+            }
+        }
+
+        return super.getToolModifiedState(state, world, pos, player, stack, toolAction);
     }
 }
