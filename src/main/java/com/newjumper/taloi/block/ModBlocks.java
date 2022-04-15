44906e6a1ff32bb3d87cecad1584a6e87f94f16d
muhta <muhtasim.nabi@gmail.com>
2022-04-15 13:24:08 -0500

    changes

--- src/main/java/com/newjumper/taloi/block/ModBlocks.java
+++ src/main/java/com/newjumper/taloi/block/ModBlocks.java
@@ -395,8 +395,8 @@ public class ModBlocks {
         return toReturn;
     }
 
-    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
-        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
     }
 
     public static void register(IEventBus eventbus) {
