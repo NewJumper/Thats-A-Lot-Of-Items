52a2499bf13342edb289e26ba6d575b5bba469a0
muhta <muhtasim.nabi@gmail.com>
2022-05-12 17:55:46 -0500

    added more to lang provider, missing advancement translations

--- src/main/java/com/newjumper/taloi/datagen/lang/ModLanguageProvider.java
+++ src/main/java/com/newjumper/taloi/datagen/lang/ModLanguageProvider.java
@@ -2,10 +2,12 @@ package com.newjumper.taloi.datagen.lang;
 
 import com.newjumper.taloi.ThatsALotOfItems;
 import com.newjumper.taloi.block.ModBlocks;
+import com.newjumper.taloi.block.entity.ModBlockEntities;
 import com.newjumper.taloi.item.ModItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.common.data.LanguageProvider;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -22,22 +24,32 @@ public class ModLanguageProvider extends LanguageProvider {
         add("itemGroup.taloi_tool_tab", "TALOI Tools");
         add("itemGroup.taloi_combat_tab", "TALOI Combat");
 
-        ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(this::addTranslation);
-        ModItems.ITEMS.getEntries().stream().map(RegistryObject::get).forEach(this::addTranslation);
+        ModBlocks.BLOCKS.getEntries().stream().map(RegistryObject::get).forEach(this::addBlock);
+        ModItems.ITEMS.getEntries().stream().map(RegistryObject::get).forEach(this::addItem);
         // ^^^^^^^ CREATES DUPLICATE KEYS FOR BLOCK ITEMS, MUST FIX ^^^^^^^
 
-        // add container translations
+        add("container.constructor", "Constructor");
+        add("container.press", "Hydraulic Press");
+        add("container.processor", "Processor");
+        add("container.separator", "Separator");
+        ModBlockEntities.BLOCK_ENTITIES.getEntries().stream().map(RegistryObject::get).forEach(this::addContainer);
+
         // add advancement translations
     }
 
-    private void addTranslation(Block block) {
+    private void addBlock(Block block) {
         String key = block.getRegistryName().getPath();
         add("block.taloi." + key, convertToName(key));
     }
 
-    private void addTranslation(Item item) {
+    private void addItem(Item item) {
         String key = item.getRegistryName().getPath();
-        add("item.taloi." + item.getRegistryName().getPath(), convertToName(key));
+        add("item.taloi." + key, convertToName(key));
+    }
+
+    private void addContainer(BlockEntityType<?> blockEntity) {
+        String key = blockEntity.getRegistryName().getPath();
+        add("container." + convertToInitials(key), convertToName(key));
     }
 
     private String convertToName(String key) {
@@ -46,11 +58,26 @@ public class ModLanguageProvider extends LanguageProvider {
         else key = "Block of " + key.substring(0, 1).toUpperCase() + key.substring(1, key.lastIndexOf('_'));
 
         StringBuilder builder = new StringBuilder(key);
-        for (int i = 0; i < builder.length(); i++) {
-            if (builder.charAt(i) == '_') {
+        for(int i = 0; i < builder.length(); i++) {
+            if(builder.charAt(i) == '_') {
                 builder.deleteCharAt(i);
                 builder.replace(i, i + 1, " " + Character.toUpperCase(builder.charAt(i)));
             }
+        }
+
+        return builder.toString();
+    }
+
+    private String convertToInitials(String key) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(key.charAt(0));
+
+        for(int i = 1; i < key.length() - 2; i++) {
+            if(key.charAt(i) == '_') {
+                builder.append(key.charAt(i + 1));
+                if(key.charAt(i + 1) == 's') builder.append(key.charAt(i + 2));
+            }
+
         }
 
         return builder.toString();
