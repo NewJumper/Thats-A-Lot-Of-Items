dc56d2a24d0691b8710fa6dae18d8e1a6b13cc35
muhta <muhtasim.nabi@gmail.com>
2022-05-31 00:14:44 -0500

    updated constructing recipe category: shows cooking time, more readable gui, etc

--- src/main/java/com/newjumper/taloi/integration/ConstructingRecipeCategory.java
+++ src/main/java/com/newjumper/taloi/integration/ConstructingRecipeCategory.java
@@ -1,23 +1,27 @@
 package com.newjumper.taloi.integration;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.newjumper.taloi.ThatsALotOfItems;
 import com.newjumper.taloi.block.ModBlocks;
 import com.newjumper.taloi.recipe.ConstructingRecipe;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.crafting.Ingredient;
-
-import javax.annotation.Nonnull;
 
 public class ConstructingRecipeCategory implements IRecipeCategory<ConstructingRecipe> {
     public final static ResourceLocation UID = new ResourceLocation(ThatsALotOfItems.MOD_ID, "constructing");
@@ -25,42 +29,66 @@ public class ConstructingRecipeCategory implements IRecipeCategory<ConstructingR
 
     private final IDrawable background;
     private final IDrawable icon;
+    private final LoadingCache<Integer, IDrawableAnimated> cachedArrows;
 
-    public ConstructingRecipeCategory(IGuiHelper helper) {
-        this.background = helper.createDrawable(TEXTURE, 0, 0, 176, 83);
-        this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM, new ItemStack(ModBlocks.ALPHA_CONSTRUCTOR.get()));
+    public ConstructingRecipeCategory(IGuiHelper guiHelper) {
+        this.background = guiHelper.createDrawable(TEXTURE, 68, 16, 78, 54);
+        this.icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.ALPHA_CONSTRUCTOR.get()));
+        this.cachedArrows = CacheBuilder.newBuilder().maximumSize(28).build(new CacheLoader<>() {
+            @Override
+            public IDrawableAnimated load(Integer time) {
+                return guiHelper.drawableBuilder(TEXTURE, 176, 14, 27, 40).buildAnimated(time, IDrawableAnimated.StartDirection.LEFT, false);
+            }
+        });
     }
 
     @Override
-    public ResourceLocation getUid() {
-        return UID;
+    public void draw(ConstructingRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack stack, double mouseX, double mouseY) {
+        getArrow().draw(stack, 21, 7);
+        drawCookTime(stack, 45);
     }
 
     @Override
-    public Class<? extends ConstructingRecipe> getRecipeClass() {
-        return ConstructingRecipe.class;
+    public void setRecipe(IRecipeLayoutBuilder builder, ConstructingRecipe recipe, IFocusGroup focuses) {
+        builder.addSlot(RecipeIngredientRole.INPUT, 1, 1).addIngredients(recipe.getIngredients().get(0));
+        builder.addSlot(RecipeIngredientRole.INPUT, 1, 37).addIngredients(recipe.getIngredients().get(1));
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 57, 19).addItemStack(recipe.getResultItem());
     }
 
-    @Override
-    public Component getTitle() {
-        return new TranslatableComponent("container.taloi.constructor");
+    protected IDrawableAnimated getArrow() {
+        return this.cachedArrows.getUnchecked(100);
+    }
+
+    protected void drawCookTime(PoseStack poseStack, int y) {
+        int cookTimeSeconds = 10;
+        TranslatableComponent timeString = new TranslatableComponent("gui.jei.category.smelting.time.seconds", cookTimeSeconds);
+        Minecraft minecraft = Minecraft.getInstance();
+        Font fontRenderer = minecraft.font;
+        int stringWidth = fontRenderer.width(timeString);
+        fontRenderer.draw(poseStack, timeString, background.getWidth() - stringWidth, y, 0xFF808080);
     }
 
     @Override
     public IDrawable getBackground() {
-        return this.background;
+        return background;
     }
-
     @Override
     public IDrawable getIcon() {
-        return this.icon;
+        return icon;
+    }
+    @Override
+    public Component getTitle() {
+        return new TranslatableComponent("container.taloi.ac");
     }
 
+    @SuppressWarnings("removal")
     @Override
-    public void setRecipe(@Nonnull IRecipeLayoutBuilder builder, @Nonnull ConstructingRecipe recipe, @Nonnull IFocusGroup focusGroup) {
-        builder.addSlot(RecipeIngredientRole.INPUT, 31, 47).addIngredients(Ingredient.of(Items.COAL));
-        builder.addSlot(RecipeIngredientRole.INPUT, 69, 17).addIngredients(recipe.getIngredients().get(0));
-        builder.addSlot(RecipeIngredientRole.INPUT, 69, 53).addIngredients(recipe.getIngredients().get(1));
-        builder.addSlot(RecipeIngredientRole.OUTPUT, 125, 35).addItemStack(recipe.getResultItem());
+    public Class<? extends ConstructingRecipe> getRecipeClass() {
+        return ConstructingRecipe.class;
+    }
+    @SuppressWarnings("removal")
+    @Override
+    public ResourceLocation getUid() {
+        return UID;
     }
 }
