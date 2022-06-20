7b0c2f8d30bf1286de583efbd2acd6ac5f5ebba5
muhta <muhtasim.nabi@gmail.com>
2022-06-19 19:42:21 -0500

    changed tree generation

--- src/main/java/com/newjumper/taloi/world/features/ModPlacedFeatures.java
+++ src/main/java/com/newjumper/taloi/world/features/ModPlacedFeatures.java
@@ -9,7 +9,7 @@ import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 public class ModPlacedFeatures {
     // TREES
-    public static final Holder<PlacedFeature> EVERGREEN_PLACED = PlacementUtils.register("evergreen_placed", ModConfiguredFeatures.EVERGREEN_SPAWN, VegetationPlacements.treePlacement(PlacementUtils.countExtra(0, 0.5f, 1)));
+    public static final Holder<PlacedFeature> EVERGREEN_PLACED = PlacementUtils.register("evergreen_placed", ModConfiguredFeatures.EVERGREEN_SPAWN, VegetationPlacements.treePlacement(PlacementUtils.countExtra(0, 0.2f, 1)));
     public static final Holder<PlacedFeature> WILLOW_PLACED = PlacementUtils.register("willow_placed", ModConfiguredFeatures.WILLOW_SPAWN, VegetationPlacements.treePlacement(PlacementUtils.countExtra(0, 0.5f, 1)));
 
     // NATURAL
--- src/main/java/com/newjumper/taloi/world/gen/ModTreeGeneration.java
+++ src/main/java/com/newjumper/taloi/world/gen/ModTreeGeneration.java
@@ -3,28 +3,28 @@ package com.newjumper.taloi.world.gen;
 import com.newjumper.taloi.ThatsALotOfItems;
 import com.newjumper.taloi.world.features.ModPlacedFeatures;
 import net.minecraft.core.Holder;
-import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
-import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import java.util.List;
-import java.util.Set;
 
 @Mod.EventBusSubscriber(modid = ThatsALotOfItems.MOD_ID)
 public class ModTreeGeneration {
     @SubscribeEvent
     public static void generateTrees(final BiomeLoadingEvent event) {
-        ResourceKey<Biome> key = ResourceKey.create(Registry.BIOME_REGISTRY, event.getName());
-        Set<BiomeDictionary.Type> types = BiomeDictionary.getTypes(key);
-
         List<Holder<PlacedFeature>> base = event.getGeneration().getFeatures(GenerationStep.Decoration.VEGETAL_DECORATION);
-        if(types.contains(BiomeDictionary.Type.CONIFEROUS)) base.add(ModPlacedFeatures.EVERGREEN_PLACED);
-        if(types.contains(BiomeDictionary.Type.SAVANNA)) base.add(ModPlacedFeatures.WILLOW_PLACED);
+
+        if(biomeCheck(event.getName(), Biomes.SNOWY_PLAINS)) base.add(ModPlacedFeatures.EVERGREEN_PLACED);
+        if(biomeCheck(event.getName(), Biomes.MEADOW)) base.add(ModPlacedFeatures.WILLOW_PLACED);
+    }
+    public static boolean biomeCheck(ResourceLocation location, ResourceKey<Biome> key){
+        return location.getPath().matches(key.location().getPath());
     }
 }
